daf2d3181e5f8c2e94152ce1732605535db548b4
Syaufy <46257169+Syauqizaidan@users.noreply.github.com>
2020-02-06 10:10:42 +0700

    06/02/2020

---
+++ Avatarlogin/Avatarlogin/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Avatarlogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(82, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(82, 320);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 299);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(82, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Login!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Avatarlogin.Properties.Resources.debut;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 185);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(356, 430);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Avatarlogin/Avatarlogin/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Avatarlogin
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        List<Image> images = new List<Image>();
+        string[] location = new string[25];
+        public Form1()
+        {
+
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            location[0] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_1.jpg";
+            location[1] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_2.jpg";
+            location[2] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_3.jpg";
+            location[3] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_4.jpg";
+            location[4] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_5.jpg";
+            location[5] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_6.jpg";
+            location[6] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_7.jpg";
+            location[7] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_8.jpg";
+            location[8] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_9.jpg";
+            location[9] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_10.jpg";
+            location[10] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_11.jpg";
+            location[11] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_12.jpg";
+            location[12] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_13.jpg";
+            location[13] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_14.jpg";
+            location[14] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_15.jpg";
+            location[15] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_16.jpg";
+            location[16] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_17.jpg";
+            location[17] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_18.jpg";
+            location[18] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_19.jpg";
+            location[19] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_20.jpg";
+            location[20] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_21.jpg";
+            location[21] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_22.jpg";
+            location[22] = @"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_user_23.jpg";
+            diapo();
+        }
+
+        private void diapo()
+        {
+            for (int i = 0; i < 23; i++) {
+                Bitmap bitmap = new Bitmap(location[i]);
+                images.Add(bitmap);
+            }
+            images.Add(Properties.Resources.textbox_user_24);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0 && textBox1.Text.Length <= 15)
+            {
+                pictureBox1.Image = images[textBox1.Text.Length - 1];
+                pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else if (textBox1.Text.Length <= 0)
+                pictureBox1.Image = Properties.Resources.debut;
+            else
+                pictureBox1.Image = images[22];
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap (@"C:\Users\RPL\Documents\Visual Studio 2008\Projects\Avatarlogin\Avatarlogin\Animation\textbox_password.png");
+            pictureBox1.Image = bm;
+            textBox2.PasswordChar = '*';
+        
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+                pictureBox1.Image = images[textBox1.Text.Length - 1];
+            else
+                pictureBox1.Image = Properties.Resources.debut;
+
+        }
+    }
+}
